28fe2f9b21730ebae735e4722342850648478e99
laurin <laurinfisher69@gmail.com>
2021-11-30 20:53:58 -0900

    Created interactive loop.

--- fractional_math/fractional_math.py
+++ fractional_math/fractional_math.py
@@ -1,2 +1,17 @@
-def calculate():
+import sys 
+
+def calculate(inputStr):
+	output = inputStr
+	print("= " + output)
 	return 0
+
+def main():
+	print("Fractional calculator. Type quit to exit.")
+	user_input = ""
+	while(user_input != "quit"):
+		user_input = input("? ")
+		calculate(user_input)
+
+if __name__ == '__main__':
+    main()
+
